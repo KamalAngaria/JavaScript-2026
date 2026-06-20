fa085623b327dcf8e5a3cc839086ca281155eaa2
KamalAngaria <kamal176048@gmail.com>
2026-06-20 20:02:04 +0530

    Learning the basic variabel and process of declaration and initilatization

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
  // # JavaScript-2026 
+ // Day 1 of learning Javascript 
+//  Today topic 
+//  What is variable in javascript?
+//  varaibel is a acontainer used to store the valuse or data  in javascript
+//  there are thre types of variables in javascript 
+// 1 var 
+// 2 let
+// 3 const
+//  lets understand and disuss how we can write variables and store data in it
+//  lets take the example of let variable 
+//  we can make variable using two types of form 
+//  example == 1 let a;
+//  2 let a = 10;
+Decalaration V/S Initialization 
+what is decalaration ?
+decalration is the process of creating a variable in js and give it a name
+Exammple -----
+
+var a; // this is how we decalre a variable in js whaat this decalartion only
+work with let and var
+what is intialiazation ?
+initalization is the proces where we give the 'first value the to variable we decalred in js
+Example -----
+var a = 10; // thsi is how we decalre and intialize a variable in js 
