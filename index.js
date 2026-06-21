638e82d5f00872e1cdd5e0cbc94c16cd704dc774
KamalAngaria <kamal176048@gmail.com>
2026-06-21 17:46:22 +0530

    Day 2  Of Learning Javascript

--- index.js
+++ index.js
@@ -12,14 +12,14 @@
 //  we can make variable using two types of form 
 //  example == 1 let a;
 //  2 let a = 10;
-Decalaration V/S Initialization 
-what is decalaration ?
-decalration is the process of creating a variable in js and give it a name
-Exammple -----
+// Decalaration V/S Initialization 
+// what is decalaration ?
+// decalration is the process of creating a variable in js and give it a name
+// Exammple -----
 
-var a; // this is how we decalre a variable in js whaat this decalartion only
-work with let and var
-what is intialiazation ?
-initalization is the proces where we give the 'first value the to variable we decalred in js
-Example -----
-var a = 10; // thsi is how we decalre and intialize a variable in js 
+// var a; // this is how we decalre a variable in js whaat this decalartion only
+// work with let and var
+// what is intialiazation ?
+// initalization is the proces where we give the 'first value the to variable we decalred in js
+// Example -----
+// var a = 10; // thsi is how we decalre and intialize a variable in js 
