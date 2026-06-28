f8d20f33bcd0d9431442b64324176089d7e4803e
KamalAngaria <kamal176048@gmail.com>
2026-06-28 20:26:14 +0530

    🔵 Section D - Switch

--- index.js
+++ index.js
@@ -711,3 +711,63 @@
 // } else {
 //     console.log("Senior Citizen");
 // }
+
+//                          🔵 Section D - Switch
+
+// Q10
+// let day = 3;
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break ;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break ;
+//     case 5: 
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Staturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break ;
+//     default:
+//         console.log("Invalid Day Number")
+// }
+
+// Q11
+// let month = 2;
+// switch (month) {
+//     case 2:
+//         console.log("february")
+//         break;
+//     default:
+//         console.log("invalid gess");
+// }
+
+// Q12
+// let grade = A;
+// switch(grade){
+//     case A:
+//         console.log("Excellent");
+//         break;
+//     case B :
+//         console.log("Good");
+//         break;
+//     case C :
+//         console.log("Average");
+//         break;
+//     case D :
+//         console.log("Fail");
+//         break;
+//         default:
+//         console.log("Invalid Grade");
+
+// }
