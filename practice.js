afce1f7f717cec0496322c2ba245653d97b4cec6
KamalAngaria <kamal176048@gmail.com>
2026-07-08 12:17:55 +0530

    Day 1 Of Js Practice

---
+++ practice.js
@@ -0,0 +1,40 @@
+// Question 1 (Easy)
+
+// let name = "Alice";
+// let age = 25;
+// console.log(typeof name);  string 
+// console.log(typeof age);  number 
+
+// Question 2 (Easy)
+// Identify the data type of each variable.
+
+// let a = 100;  number
+// let b = "100";  string
+// let c = true; Boolean
+// let d = null;  null
+// let e = undefined; undefined
+// let f = 10n; BigInt
+
+// Question 3 (Variables)
+
+// const city = "Delhi";
+// city = "Mumbai";
+                        // TypeError
+// console.log(city);
+
+// Question 4 (Predict the Output)
+// let x = 10;
+// let y = "20";
+
+// console.log(x + y);  1020
+// console.log(typeof (x + y));  String
+
+// Question 5 (Challenge)
+
+// let a;
+// let b = null;
+
+// console.log(typeof a); undefined
+// console.log(typeof b); Object
+// console.log(a == b); true
+// console.log(a === b); false
